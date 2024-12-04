d35f911d89dd02a83f4acdfae845c187dd6915a5
Benjamin Crozat <hello@benjamincrozat.com>
2024-12-04 16:18:19 +0100

    WIP

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -6,11 +6,20 @@
 
         <title>{{ $title ?? config('app.name') }}</title>
 
+        @if (app()->isProduction())
+            <script defer src="https://api.pirsch.io/pa.js" id="pianjs" data-code="HXdkUoyfzOakKngFs6IwJUaR7WryTzx3"></script>
+        @endif
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900" />
+
+        <link rel="icon" type="image/png" href="{{ Vite::asset('resources/img/favicon-96x96.png') }}" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="{{ Vite::asset('resources/img/favicon.svg') }}" />
+        <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" />
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/img/apple-touch-icon.png') }}" />
     </head>
     <body {{ $attributes->class('bg-gray-900 font-light text-gray-300') }}>
         {{ $slot }}
