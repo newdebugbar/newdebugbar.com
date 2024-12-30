321b757b005d8751f0dda66a8d6b3d200f888b0f
Benjamin Crozat <hello@benjamincrozat.com>
2024-12-30 17:45:04 +0100

    WIP

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -38,9 +38,9 @@
             </a>
 
             <nav class="container flex items-center justify-end gap-8 mt-8 text-sm md:text-base xl:max-w-screen-lg">
-                <a href="#roadmap" data-pirsch-event='Clicked "Roadmap" in the nav bar' class="font-medium">Roadmap</a>
-
                 <a href="#sponsors" data-pirsch-event='Clicked "Sponsors" in the nav bar' class="font-medium">Sponsors</a>
+
+                <a href="#roadmap" data-pirsch-event='Clicked "Roadmap" in the nav bar' class="font-medium">Roadmap</a>
 
                 <a href="https://github.com/newdebugbar" target="_blank" data-pirsch-event="Clicked GitHub in the nav bar" class="fill-current">
                     <x-icon-github class="size-4 md:size-5" />
