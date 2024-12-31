74fb4e41918fa4e5fd720c7a7facf0a5c3b1ea2d
Benjamin Crozat <hello@benjamincrozat.com>
2024-12-31 15:33:29 +0100

    CS fix

--- database/migrations/2024_12_07_135324_create_telescope_entries_table.php
+++ database/migrations/2024_12_07_135324_create_telescope_entries_table.php
@@ -1,15 +1,15 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
     /**
      * Get the migration connection name.
      */
-    public function getConnection(): ?string
+    public function getConnection() : ?string
     {
         return config('telescope.storage.database.connection');
     }
@@ -17,7 +17,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up() : void
     {
         $schema = Schema::connection($this->getConnection());
 
@@ -59,7 +59,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down() : void
     {
         $schema = Schema::connection($this->getConnection());
 
