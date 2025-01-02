88ee7369beb4aeb7914c915b149a24916677d531
Benjamin Crozat <hello@benjamincrozat.com>
2025-01-02 16:01:01 +0100

    Send proper 404 codes

--- app/Http/Controllers/Documentation/ListDocumentsController.php
+++ app/Http/Controllers/Documentation/ListDocumentsController.php
@@ -11,9 +11,15 @@ class ListDocumentsController extends Controller
 {
     public function __invoke(string $version) : View
     {
+        $path = resource_path("docs/$version/00-introduction.md");
+
+        if (! File::exists($path)) {
+            abort(404);
+        }
+
         $navigation = app(ListDocuments::class)->list($version);
 
-        $content = File::get(resource_path("docs/$version/00-introduction.md"));
+        $content = File::get($path);
 
         return view('docs.index', compact('navigation', 'content'));
     }
--- app/Http/Controllers/Documentation/ShowDocumentController.php
+++ app/Http/Controllers/Documentation/ShowDocumentController.php
@@ -11,11 +11,19 @@ class ShowDocumentController extends Controller
 {
     public function __invoke(string $version, string $slug) : View
     {
+        $directory = resource_path("docs/$version");
+
+        $filePath = data_get(glob("$directory/*$slug.md"), 0);
+
+        if (! File::exists($directory) || empty($filePath)) {
+            abort(404);
+        }
+
         $navigation = app(ListDocuments::class)->list($version);
 
         $title = $navigation->firstWhere('slug', $slug)['title'];
 
-        $content = File::get(glob(resource_path("docs/$version/*$slug.md"))[0]);
+        $content = File::get($filePath);
 
         return view('docs.show', compact('navigation', 'title', 'slug', 'content'));
     }
