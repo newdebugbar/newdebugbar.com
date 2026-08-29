b25d85bfd152ddb069583497f434f9fb6bd29d3c
Benjamin Crozat <hello@benjamincrozat.com>
2026-08-29 21:42:45 +0200

    Correct mobile screenshot captures
    - Capture reusable light and dark inspector assets from a 402x874 DPR-2 viewport.
    - Store only the tight product surface at its natural 804x1432 dimensions.
    - Remove the mobile display crop and use the real asset aspect ratio.

--- resources/views/components/screenshots/request-inspector.blade.php
+++ resources/views/components/screenshots/request-inspector.blade.php
@@ -4,10 +4,10 @@
     'fetchpriority' => null,
 ])
 
-<picture {{ $attributes->class('block aspect-[1536/780] max-[47.999rem]:aspect-[402/717] max-[47.999rem]:overflow-hidden') }} data-request-inspector-screenshot>
+<picture {{ $attributes->class('block aspect-[1536/780] max-[47.999rem]:aspect-[804/1432]') }} data-request-inspector-screenshot>
     <source media="(max-width: 47.999rem)" data-request-inspector-mobile-source>
     <img
-        class="block h-auto w-full drop-shadow-[0_1.75rem_2.5rem_rgb(24_10_58_/_17%)] max-[47.999rem]:h-full max-[47.999rem]:object-cover max-[47.999rem]:object-bottom dark:drop-shadow-[0_1.75rem_2.5rem_rgb(0_0_0_/_48%)]"
+        class="block h-auto w-full drop-shadow-[0_1.75rem_2.5rem_rgb(24_10_58_/_17%)] dark:drop-shadow-[0_1.75rem_2.5rem_rgb(0_0_0_/_48%)]"
         data-request-inspector-image
         width="1536"
         height="780"
