31ba2d5ec41dfb6682abb301be70e0ff93383aee
Benjamin Crozat <hello@benjamincrozat.com>
2026-09-03 18:14:18 +0200

    Refine the social preview
    - Replace the metaphor headline with a clear debugging outcome.
    
    - Remove defined background shapes and restore safe text margins.
    
    - Export the exact canvas origin with balanced product framing.

--- resources/views/social/og-image.blade.php
+++ resources/views/social/og-image.blade.php
@@ -29,35 +29,20 @@
             data-social-preview-height="630"
         >
             <div
-                class="absolute inset-0 z-[-30] [background:linear-gradient(112deg,transparent_30%,rgb(124_58_237_/_13%)_53%,transparent_73%),radial-gradient(ellipse_680px_390px_at_78%_24%,rgb(139_92_246_/_30%),transparent_70%),radial-gradient(ellipse_560px_290px_at_8%_104%,rgb(76_29_149_/_24%),transparent_72%),#07070a]"
+                class="absolute inset-0 z-[-30] [background:radial-gradient(140%_125%_at_92%_8%,rgb(139_92_246_/_30%)_0%,rgb(91_33_182_/_16%)_34%,transparent_68%),radial-gradient(125%_90%_at_16%_104%,rgb(76_29_149_/_17%)_0%,transparent_64%),#07070a]"
                 aria-hidden="true"
             ></div>
 
-            <div
-                class="absolute inset-0 z-[-20] opacity-[0.34] [background-image:linear-gradient(rgb(255_255_255_/_6%)_1px,transparent_1px),linear-gradient(90deg,rgb(255_255_255_/_6%)_1px,transparent_1px)] [background-size:48px_48px] [-webkit-mask-image:radial-gradient(circle_at_84%_18%,black,transparent_58%)] [mask-image:radial-gradient(circle_at_84%_18%,black,transparent_58%)]"
-                aria-hidden="true"
-            ></div>
-
-            <div
-                class="absolute -top-[506px] -right-[264px] z-[-10] size-[760px] rounded-full border border-violet-200/15 shadow-[0_0_0_86px_rgb(196_181_253_/_4%),0_0_0_172px_rgb(196_181_253_/_2%)]"
-                aria-hidden="true"
-            ></div>
-
-            <h1 class="absolute top-14 left-16 z-10 m-0 w-[1040px] text-[78px] leading-[0.94] [font-weight:650] tracking-[-0.058em] text-zinc-50">
-                Laravel,<br>
-                show your <span class="text-violet-300 [text-shadow:0_0_38px_rgb(124_58_237_/_30%)]">work.</span>
+            <h1 class="absolute top-16 left-20 z-10 m-0 w-[1040px] text-[76px] leading-[0.95] [font-weight:650] tracking-[-0.052em] text-zinc-50">
+                Debug Laravel<br>
+                without the <span class="text-violet-300 [text-shadow:0_0_38px_rgb(124_58_237_/_30%)]">guesswork.</span>
             </h1>
 
-            <p class="absolute top-[232px] left-[68px] z-10 m-0 w-[980px] text-[27px] leading-[1.35] [font-weight:450] tracking-[-0.018em] text-zinc-300">
+            <p class="absolute top-[239px] left-[84px] z-10 m-0 w-[1000px] text-[26px] leading-[1.35] [font-weight:450] tracking-[-0.018em] text-zinc-300">
                 See what happened. Give your coding agent the same evidence.
             </p>
 
-            <div
-                class="absolute top-[296px] -left-5 z-0 h-[330px] w-[1240px] rotate-[-1.1deg] bg-[radial-gradient(ellipse_at_52%_8%,rgb(196_181_253_/_50%)_0%,rgb(124_58_237_/_31%)_36%,rgb(76_29_149_/_14%)_55%,transparent_74%)] blur-[30px]"
-                aria-hidden="true"
-            ></div>
-
-            <div class="absolute top-[350px] left-5 z-10 w-[1160px] origin-top rounded-[21px] bg-[linear-gradient(104deg,rgb(255_255_255_/_32%),rgb(196_181_253_/_62%)_44%,rgb(255_255_255_/_14%))] p-0.5 [box-shadow:0_34px_76px_rgb(0_0_0_/_72%),0_0_58px_rgb(124_58_237_/_32%)] [transform:perspective(1400px)_rotateX(2deg)_rotateZ(-1.1deg)]">
+            <div class="absolute top-[350px] left-[72px] z-10 w-[1056px] origin-top rounded-[21px] bg-[linear-gradient(104deg,rgb(255_255_255_/_32%),rgb(196_181_253_/_62%)_44%,rgb(255_255_255_/_14%))] p-0.5 [box-shadow:0_38px_84px_rgb(0_0_0_/_74%),0_0_62px_rgb(124_58_237_/_29%)] [transform:perspective(1600px)_rotateX(2deg)_rotateZ(-1.05deg)]">
                 <div class="overflow-hidden rounded-[19px] bg-[#0b0b0e] shadow-[inset_0_1px_rgb(255_255_255_/_8%)]">
                     <img
                         class="block h-auto w-full"
